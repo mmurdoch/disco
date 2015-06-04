a675fbda173f48c01048f7cdcabc042c64dd864c
mmurdoch <matthew.murdoch.0@gmail.com>
2015-06-04 22:02:28 +0100

    Support multiple broadcast listeners

--- broadcast_client.py
+++ broadcast_client.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import socket
 
 address = ('<broadcast>', 54545)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+broadcasting_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcasting_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 to_send = ""
-client_socket.sendto(to_send, address)
-received, server_address = client_socket.recvfrom(2048)
-print server_address[0]
+broadcasting_socket.sendto(to_send, address)
+#received, receiver_address = broadcasting_socket.recvfrom(2048)
+#print("Receiver address: " + receiver_address[0])
--- broadcast_listener.py
+++ broadcast_listener.py
@@ -1,11 +1,14 @@
+from __future__ import print_function
 import socket
-address = ('', 54545)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-server_socket.bind(address)
 
+address = ('', 54545)
+listening_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+listening_socket.bind(address)
+
+print("Listening for broadcasts...")
 while True:
-    print "Listening"
-    recv_data, addr = server_socket.recvfrom(2048)
-    print addr,':',recv_data
-    server_socket.sendto("*"+recv_data, addr)
+    recv_data, addr = listening_socket.recvfrom(2048)
+    print("Received '" + recv_data + "' from " + str(addr))
+    #listening_socket.sendto("*" + recv_data, addr)
