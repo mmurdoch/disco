b31822a3ce493c58db9e662ac686adac8bbc1d00
mmurdoch <matthew.murdoch.0@gmail.com>
2015-06-06 18:02:52 +0100

    Changed Python code order to be consistent with C

--- broadcaster.py
+++ broadcaster.py
@@ -1,11 +1,8 @@
 from __future__ import print_function
 import socket
 
-address = ('<broadcast>', 54545)
 broadcasting_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 broadcasting_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-to_send = ""
-broadcasting_socket.sendto(to_send, address)
-#received, receiver_address = broadcasting_socket.recvfrom(2048)
-#print("Receiver address: " + receiver_address[0])
+address = ('<broadcast>', 54545)
+broadcasting_socket.sendto('', address)
--- listener.py
+++ listener.py
@@ -2,12 +2,14 @@ from __future__ import print_function
 import socket
 
 try:
-    broadcast_address = ('', 54545)
     listening_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    broadcast_address = ('', 54545)
     listening_socket.bind(broadcast_address)
     print('Listening for broadcasts...')
+
     while True:
         recv_data, addr = listening_socket.recvfrom(2048)
         print('Received "' + recv_data + '" from ' + str(addr))
