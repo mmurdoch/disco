1fb6a4ebc07a70e12cfec489f7164a8608e1854d
mmurdoch <matthew.murdoch.0@gmail.com>
2015-06-06 15:27:31 +0100

    Add Ctrl+C handling to broadcast listener

--- broadcast_listener.py
+++ broadcast_listener.py
@@ -1,14 +1,16 @@
 from __future__ import print_function
 import socket
 
-address = ('', 54545)
-listening_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-listening_socket.bind(address)
-
-print("Listening for broadcasts...")
-while True:
-    recv_data, addr = listening_socket.recvfrom(2048)
-    print("Received '" + recv_data + "' from " + str(addr))
-    #listening_socket.sendto("*" + recv_data, addr)
+try:
+    address = ('', 54545)
+    listening_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listening_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    listening_socket.bind(address)
+    print('Listening for broadcasts...')
+    while True:
+        recv_data, addr = listening_socket.recvfrom(2048)
+        print('Received "' + recv_data + '" from ' + str(addr))
+        #listening_socket.sendto("*" + recv_data, addr)
+except KeyboardInterrupt:
+    pass
