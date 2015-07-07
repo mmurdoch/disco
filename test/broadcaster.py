2752eb263d11408b706323babe3c912c1be07afe
mmurdoch <matthew.murdoch.0@gmail.com>
2015-07-07 19:49:47 +0100

    Remove Python versions

--- test/broadcaster.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import socket
-
-broadcasting_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-broadcasting_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-address = ('<broadcast>', 54545)
-broadcasting_socket.sendto('', address)
