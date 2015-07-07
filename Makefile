352ba0ee652ca9484695636fd5d88ba1c1ba80dc
mmurdoch <matthew.murdoch.0@gmail.com>
2015-07-07 21:05:27 +0100

    Move broadcast functions into own file

--- Makefile
+++ Makefile
@@ -8,6 +8,6 @@ all:
 	gcc test/time-test.c src/detest/detest.c src/disco/time.c -I src -o build/test/time-test
 	build/test/time-test
 
-	gcc test/broadcaster.c src/disco/udp.c src/disco/time.c -I src -o build/test/broadcaster
+	gcc test/broadcaster.c src/disco/udp.c src/disco/time.c src/disco/broadcast.c -I src -o build/test/broadcaster
 	gcc test/listener.c src/disco/udp.c src/disco/time.c -I src -o build/test/listener
 	python test/test.py
--- test/broadcaster.c
+++ test/broadcaster.c
@@ -2,43 +2,7 @@
 #include <stdlib.h>
 
 #include <disco/udp.h>
-
-void destroy_broadcast_socket(udp_socket_t* broadcast_socket);
-
-/**
- * Creates a socket for broadcasting messages.
- *
- * @param port the port to which to broadcast
- * @param broadcast_socket on success populated with the created socket
- * @return 0 on success, non-zero on failure
- */
-int create_broadcast_socket(udp_socket_t* broadcast_socket, int port) {
-    char* broadcast_ip = "255.255.255.255";
-
-    if (create_udp_socket(broadcast_socket, broadcast_ip, port) != 0) {
-        return -1;
-    }
-
-    if (turn_on_socket_option(*broadcast_socket, SO_BROADCAST) != 0) {
-        destroy_broadcast_socket(broadcast_socket);
-        return -1;
-    }
-
-    return 0;
-}
-
-/**
- * Destroys a socket created via create_broadcast_socket.
- *
- * @param broadcast_socket the socket to destroy
- */
-void destroy_broadcast_socket(udp_socket_t* broadcast_socket) {
-    destroy_udp_socket(broadcast_socket);
-}
-
-int broadcast_message(udp_socket_t broadcast_socket, const char* message) {
-    return send_message(broadcast_socket, message);
-}
+#include <disco/broadcast.h>
 
 int main(int argc, char *argv[]) {
     udp_socket_t broadcast_socket;
