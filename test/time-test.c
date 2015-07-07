7df596443ac992039d239aa34212ff83943f6d7f
mmurdoch <matthew.murdoch.0@gmail.com>
2015-07-07 19:30:35 +0100

    Add milliseconds to timeval test

--- test/time-test.c
+++ test/time-test.c
@@ -28,10 +28,19 @@ void test_one_thousand_milliseconds_to_timeval() {
     expect(t.tv_sec == 1 && t.tv_usec == 0);
 }
 
+void test_one_thousand_and_one_milliseconds_to_timeval() {
+    size_t milliseconds = 1001;
+
+    struct timeval t = milliseconds_to_timeval(milliseconds);
+
+    expect(t.tv_sec == 1 && t.tv_usec == 1000);
+}
+
 int main() {
     add_test(test_zero_milliseconds_to_timeval);
     add_test(test_one_millisecond_to_timeval);
     add_test(test_one_thousand_milliseconds_to_timeval);
+    add_test(test_one_thousand_and_one_milliseconds_to_timeval);
 
     return run_tests();
 }
