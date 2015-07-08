b4398f967a9cdf8c51e69ffa421a1ec0ca1e79ee
mmurdoch <matthew.murdoch.0@gmail.com>
2015-07-08 22:43:21 +0100

    Change broadcaster exit code handling for consistency

--- test/broadcaster.c
+++ test/broadcaster.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[]) {
 
     if (create_broadcast_socket(&broadcast_socket, port) != 0) {
         fprintf(stderr, "Failed to create broadcasting socket\n");
-        exit(1);
+        return 1;
     }
 
     if (broadcast_message(&broadcast_socket, "DSC10HERE") != 0) {
         fprintf(stderr, "Failed to send broadcast message\n");
         destroy_broadcast_socket(&broadcast_socket);
-        exit(1);
+        return 1;
     }
 
     destroy_broadcast_socket(&broadcast_socket);
